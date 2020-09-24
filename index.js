4f668c57c870084f27534b5fe1efe2ab1ad8f858
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-24 02:53:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function BoardMember(name, homeState, training) {
   this.name = name
   this.homeState = homeState
   this.training = training
-  this.veto = function(){
-    return `No, i must disagree`
-  }
+}
+
+BoardMember.prototype.veto = function(){
+  return `No, i must disagree`
 }
