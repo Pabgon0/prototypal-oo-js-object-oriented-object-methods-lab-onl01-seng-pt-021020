884d5d75b47caca2a111e12b2cafa0687e31a4af
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-24 02:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ BoardMember.prototype.doCharity = function(){
   return "I like to help people."
 }
 
-BoardMember.prototype.veto = function(){
+BoardMember.prototype.releasePressStatement = function(){
   return "No, i must disagree"
 }
 
